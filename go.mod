3a64801fcf156e345b85bfec34da12c94f315e56
milgradesec <38557680+milgradesec@users.noreply.github.com>
2022-07-25 00:58:58 +0200

    use spg13/viper for config

--- go.mod
+++ go.mod
@@ -3,22 +3,31 @@ module github.com/milgradesec/pages-gc
 go 1.18
 
 require (
-	github.com/AlecAivazis/survey/v2 v2.3.5
 	github.com/cloudflare/cloudflare-go v0.45.0
+	github.com/spf13/viper v1.12.0
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
-	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
-	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	gopkg.in/ini.v1 v1.66.6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/AlecAivazis/survey/v2"
 	"github.com/cloudflare/cloudflare-go"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -16,17 +15,34 @@ var (
 )
 
 func main() {
-	flag.StringVar(&email, "email", "", "Cloudflare account email")
-	flag.StringVar(&apikey, "key", "", "Cloudflare API Key")
-	flag.StringVar(&account, "account", "", "Cloudflare account ID")
-	flag.StringVar(&project, "project", "", "Pages project name")
-	flag.Parse()
+	viper.SetConfigFile(".env")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok { //nolint
+			fmt.Println("Config file not found.")
+		} else {
+			fmt.Printf("Error reading config file: %v\n", err)
+			os.Exit(1)
+		}
+	}
 
-	if email == "" || apikey == "" || account == "" {
-		email = os.Getenv("CLOUDFLARE_EMAIl")
-		apikey = os.Getenv("CLOUDFLARE_API_KEY")
-		account = os.Getenv("CLOUDFLARE_ACCOUNT_ID")
-		project = os.Getenv("CLOUDFLARE_PAGES_PROJECT")
+	if !viper.IsSet("CLOUDFLARE_EMAIL") {
+		fmt.Println("CLOUDFLARE_EMAIL not set")
+	}
+	if !viper.IsSet("CLOUDFLARE_API_KEY") {
+		fmt.Println("CLOUDFLARE_API_KEY not set")
+	}
+	if !viper.IsSet("CLOUDFLARE_ACCOUNT_ID") {
+		fmt.Println("CLOUDFLARE_ACCOUNT_ID not set")
+	}
+
+	email = viper.GetString("CLOUDFLARE_EMAIl")
+	apikey = viper.GetString("CLOUDFLARE_API_KEY")
+	account = viper.GetString("CLOUDFLARE_ACCOUNT_ID")
+
+	if viper.IsSet("CLOUDFLARE_PAGES_PROJECT") {
+		project = viper.GetString("CLOUDFLARE_PAGES_PROJECT")
+	} else {
+		// TODO: ask user
 	}
 
 	api, err := cloudflare.New(apikey, email)
@@ -34,7 +50,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if project == "" {
+	if project == "all" {
+		err := purgeAllProjects(api)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	/*if project == "" {
 		projects, _, err := api.ListPagesProjects(context.TODO(), account, cloudflare.PaginationOptions{})
 		if err != nil {
 			log.Fatal(err)
@@ -71,5 +93,40 @@ func main() {
 			continue
 		}
 		fmt.Printf("🧹 Deleted deployment id=%s\n", d.ID)
+	}*/
+}
+
+func purgeAllProjects(api *cloudflare.API) error {
+	projects, _, err := api.ListPagesProjects(context.TODO(), account, cloudflare.PaginationOptions{})
+	if err != nil {
+		return err
 	}
+
+	for _, p := range projects {
+		err = purgeProject(api, p.Name)
+		if err != nil {
+			fmt.Printf("❌ Failed to cleanup project '%s': %v", p.Name, err)
+		}
+	}
+	return nil
+}
+
+func purgeProject(api *cloudflare.API, name string) error {
+	opts := cloudflare.ListPagesDeploymentsParams{
+		ProjectName: name,
+	}
+	deployments, _, err := api.ListPagesDeployments(context.TODO(), cloudflare.AccountIdentifier(account), opts)
+	if err != nil {
+		return err
+	}
+
+	for _, d := range deployments {
+		err = api.DeletePagesDeployment(context.TODO(), cloudflare.AccountIdentifier(account), project, d.ID)
+		if err != nil {
+			fmt.Printf("❌ Failed to delete deployment id=%s project=%s\n", d.ID, d.ProjectName)
+			continue
+		}
+		fmt.Printf("🧹 Deleted deployment id=%s project=%s\n", d.ID, d.ProjectName)
+	}
+	return nil
 }
